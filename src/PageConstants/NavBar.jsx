c41d4c5e4636a4a2ff62c11250d983e149b4a0c1
Becca Staple <petite.becca.s@gmail.com>
2020-09-02 17:21:45 +0100

    search bar in nav bar deleted

--- src/PageConstants/NavBar.jsx
+++ src/PageConstants/NavBar.jsx
@@ -21,10 +21,7 @@ export default class NavBar extends React.Component {
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+                        
                     </Navbar.Collapse>
                 </Navbar>
             </div>
